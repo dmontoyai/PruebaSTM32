d2f1bab41a4b09f97749535d4118b9439267a0a0
David Montoya <d.montoya.i@gmail.com>
2023-06-07 01:13:39 +0200

    feat: Se lee el ADC 16 muestras mediante DMA.

---
+++ Core/Inc/main.h
@@ -0,0 +1,85 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2023 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define NCS_MEM_SPI_Pin GPIO_PIN_1
+#define NCS_MEM_SPI_GPIO_Port GPIOC
+#define MEMS_INT1_Pin GPIO_PIN_1
+#define MEMS_INT1_GPIO_Port GPIOA
+#define MEMS_INT2_Pin GPIO_PIN_2
+#define MEMS_INT2_GPIO_Port GPIOA
+#define ACP_RST_Pin GPIO_PIN_7
+#define ACP_RST_GPIO_Port GPIOA
+#define ADC3DGB_Pin GPIO_PIN_1
+#define ADC3DGB_GPIO_Port GPIOG
+#define TP_INT1_Pin GPIO_PIN_15
+#define TP_INT1_GPIO_Port GPIOA
+#define LD3_Pin GPIO_PIN_13
+#define LD3_GPIO_Port GPIOG
+#define LD4_Pin GPIO_PIN_14
+#define LD4_GPIO_Port GPIOG
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
